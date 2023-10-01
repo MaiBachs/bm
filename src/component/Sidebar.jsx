0662d90eac69baa1050d3902ab25de85767a6b6b
Do Duc Dien <dien2k1tt@gmail.com>
2023-10-01 16:11:21 +0700

    sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <div className="mx-1 p-1 bg-red-200">
-      <Link to="/" className="flex items-center ">
+    <div className="mx-1 p-1  bg-red-200">
+      <Link to="/" className="flex items-center my-2 gap-3">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -22,7 +22,7 @@ function Sidebar() {
         Trang chủ
       </Link>
       <div className="bg-gray-100/60 h-[0.05rem] mt-1"></div>
-      <Link to="/" className="flex items-center my-1 ">
+      <Link to="/" className="flex items-center my-2 gap-3 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -34,10 +34,64 @@ function Sidebar() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
           />
         </svg>
-        Tra cứu
+        Giao dịch liên ngân hàng
+      </Link>
+      <div className="bg-gray-100/60 h-[0.05rem] mt-1"></div>
+      <Link className="flex items-center my-2 gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z"
+          />
+        </svg>
+        Giao dịch nội bộ
+      </Link>
+      <div className="bg-gray-100/60 h-[0.05rem] mt-1"></div>
+      <Link className="flex items-center my-2 gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+          />
+        </svg>
+        Biểu đồ chứng khoán
+      </Link>
+      <div className="bg-gray-100/60 h-[0.05rem] mt-1"></div>
+      <Link className="flex items-center my-2 gap-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
+          />
+        </svg>
+        Quản lý thẻ
       </Link>
     </div>
   );
