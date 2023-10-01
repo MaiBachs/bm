7637bb28518826ce9d6ae7450ecad5339a08ed9d
Do Duc Dien <dien2k1tt@gmail.com>
2023-10-01 12:31:08 +0700

    ...

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
-  return <div>Bach 1000000</div>;
+  return (
+    <div className="mx-1 p-1 bg-red-200">
+      <Link to="/" className="flex items-center ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        Trang chủ
+      </Link>
+      <div className="bg-gray-100/60 h-[0.05rem] mt-1"></div>
+      <Link to="/" className="flex items-center my-1 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+          />
+        </svg>
+        Tra cứu
+      </Link>
+    </div>
+  );
 }
 
 export default Sidebar;
