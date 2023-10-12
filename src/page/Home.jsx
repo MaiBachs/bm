e9a858d3819c443abb5892be86824524d252082f
Do Duc Dien <dien2k1tt@gmail.com>
2023-10-12 08:19:26 +0700

    home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,33 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex justify-center mx-auto py-1 grid grid-cols-4 mt-8 bg-white">
-      <form className="col-start-2 col-span-2">
-        <div className="flex rounded-sm bg-gray-100 p-1">
-          <input
-            type="text"
-            className="flex-grow border-none bg-transparent px-3 py-2 text-black outline-none"
-            placeholder="Tìm kiếm "
-          />
-          <button className="flex-shrink-0 rounded-sm bg-blue-100 py-2 px-6 hover:opacity-90">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          </button>
+    <div className="mx-auto bg-white">
+      <div className="py-1 grid grid-cols-6 gap-3 mt-8">
+        <div className="p-5 col-span-6 rounded-sm shadow-lg flex justify-center bg-gray-100">
+          {" "}
+          Tổng giao dịch :
         </div>
-      </form>
+      </div>
+      <div className="mt-3 p-1 grid grid-cols-6 gap-5 justify-center">
+        <div className="col-span-2">
+          <Link
+            to="/InterbankTransactions"
+            className="p-5 mt-3 rounded-sm shadow-lg flex justify-start bg-gray-100"
+          >
+            Giao dịch liên ngân hàng :{" "}
+          </Link>
+        </div>
+
+        <div className="col-span-2">
+          <Link
+            to="/InternalTransactions"
+            className="p-5 mt-3 rounded-sm shadow-lg flex justify-start bg-gray-100"
+          >
+            Giao dịch nội bộ :{" "}
+          </Link>
+        </div>
+        <div className="col-span-2">
+          <Link
+            to=""
+            className="p-5 mt-3 rounded-sm shadow-lg flex justify-start bg-gray-100"
+          >
+            Lãi suất :{" "}
+          </Link>
+        </div>
+        <div className="col-span-2">
+          <Link
+            to="/card"
+            className="p-5 mt-3 rounded-sm shadow-lg flex justify-start bg-gray-100"
+          >
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>Thẻ mới : 10 </th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array(10)
+                  .fill(0)
+                  .map((_, index) => (
+                    <tr key={index}>
+                      <td>Do Duc Dien</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
