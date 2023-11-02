c11fdb93b44c05fdb9a1e0e82aab8deece4a1ee3
Do Duc Dien <dien2k1tt@gmail.com>
2023-11-02 20:50:47 +0700

    repaid git commit -m repaid git commit -m repaid

---
+++ src/page/PopoverRepaid.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Popover = ({ show, onClose, children }) => {
+  if (!show) return null;
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+      <div className="absolute w-90% bg-white p-4 shadow-lg rounded-lg">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Popover;
